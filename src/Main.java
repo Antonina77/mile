cca8188ed292707fd2eda165f27d8bf2fc9d57f9
Antonina Yazykova <adrianery94@>
2020-11-02 15:16:13 +0300

    java commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int cost = 1234;
         int mile = 1;
-        int total = cost/20;
+        int total = cost / 20;
         System.out.println(total);
 
-        }
     }
+}
